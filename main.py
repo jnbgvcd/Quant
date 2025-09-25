25e952b72847d678e2b1cd90ca31c7158cedab55
luoxq <905057671@qq.com>
2025-09-25 23:16:49 +0800

    构建qss文件

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import sys
 from PySide6.QtWidgets import QApplication
+from PySide6.QtCore import QFile, QTextStream
 from MainWindowUI import *
 
 class MainWindow(QMainWindow,Ui_MainWindow):
@@ -7,8 +8,21 @@ class MainWindow(QMainWindow,Ui_MainWindow):
         super(MainWindow,self).__init__(parent)
         self.setupUi(self)
 
+def load_stylesheet(filename):
+    """加载并返回QSS样式文件内容"""
+    file = QFile(filename)
+    if file.open(QFile.ReadOnly | QFile.Text):
+        stream = QTextStream(file)
+        return stream.readAll()
+    return ""
+
 if __name__=="__main__":
     app=QApplication(sys.argv)
+    # 加载并应用样式表
+    stylesheet = load_stylesheet("styles.qss")
+    if stylesheet:
+        app.setStyleSheet(stylesheet)
+
     mainWindow=MainWindow()
     mainWindow.show()
-    sys.exit(app.exec_())
+    sys.exit(app.exec())
